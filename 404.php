e20595f9c8d1158b9fe741cdc142b993e44d7671
mitkimi <mitkimi@163.com>
2018-07-12 19:13:28 +0800

    完成一些基础样式

--- 404.php
+++ 404.php
@@ -1,16 +1,16 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
-
-    <div class="col-mb-12 col-tb-8 col-tb-offset-2">
-
-        <div class="error-page">
-            <h2 class="post-title">404 - <?php _e('页面没找到'); ?></h2>
-            <p><?php _e('你想查看的页面已被转移或删除了, 要不要搜索看看: '); ?></p>
-            <form method="post">
-                <p><input type="text" name="s" class="text" autofocus /></p>
-                <p><button type="submit" class="submit"><?php _e('搜索'); ?></button></p>
-            </form>
+<div class="layui-container theme-container">
+    <div class="layui-breadcrumb">
+      <a href="/"><?php $this->options->title() ?></a>
+      <a><cite>404</cite></a>
+    </div>
+    <div class="nothing-container">
+        <div class="pic-container">
+            <img src="<?php $this->options->themeUrl('./img/404.png'); ?>" style="width: 50%;height: 50%"/>
         </div>
-
-    </div><!-- end #content-->
+        <p>没有此页面</p>
+    </div>
+</div>
+    <?php $this->need('sidebar.php'); ?>
 	<?php $this->need('footer.php'); ?>
--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * 用于开发网站的typecho主题，力求简约
+ * 一个用来作为开发团队工作沟通整合用的网站主题
  * UI库：layui
  * 插件：layui.js swiper.js highlight.js(cdn)
+ * 未完成内容：评论区的显示和体验、阴影部分的体验、大量文章显示懒加载、首页轮播图后台设置、工具栏的整合、快速入口后台设定
+ * 命名：Thomas & mitkimi 首三字母
  * 
- * @package Dev_site_theme
+ * @package Thomit
  * @author i@mitkimi.com
- * @version 1.1
+ * @version 1.2
  * @link https://github.com/mitkimi/devsite_theme
  */
 
